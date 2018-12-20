cbfc115c1f52f936301d09b18b80b36d529486fe
i.valchev <i.valchev@eltrade.com>
2018-12-20 17:11:20 +0200

    Added first projects

---
+++ tests/MushroomCloud.Services.Identity.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace MushroomCloud.Services.Identity.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
