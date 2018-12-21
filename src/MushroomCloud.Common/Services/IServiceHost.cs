111086fa8e8d005eb98772d32f48ff20833f6800
i.valchev <i.valchev@eltrade.com>
2018-12-21 14:18:12 +0200

    Added some Services

---
+++ src/MushroomCloud.Common/Services/IServiceHost.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MushroomCloud.Common.Services
+{
+   public interface IServiceHost
+    {
+        Task RunAsync();
+
+        void Run();
+    }
+}
