87ee2b58568a2f2f5b9d9201daf635b541e9056d
i.valchev <i.valchev@eltrade.com>
2019-01-09 17:14:50 +0200

    fixed rawrabbit references

--- src/MushroomCloud.Api/Startup.cs
+++ src/MushroomCloud.Api/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using RawRabbit.vNext;
 
 namespace MushroomCloud.Api
 {
@@ -26,6 +27,7 @@ namespace MushroomCloud.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddRawRabbit();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
--- src/MushroomCloud.Common/Services/HostedService.cs
+++ src/MushroomCloud.Common/Services/HostedService.cs
@@ -15,9 +15,7 @@ namespace MushroomCloud.Common.Services
     public class HostedService : IHostedService
     {
         private readonly IWebHost _webHost;
-        private Task _executingTask;
-        private CancellationTokenSource _cts;
-
+        
         public HostedService(IWebHost webHost)
         {
             _webHost = webHost;
@@ -25,37 +23,7 @@ namespace MushroomCloud.Common.Services
         public Task RunAsync() => _webHost.RunAsync();
 
         public void Run() => _webHost.Run();
-        // protected Task ExecuteAsync(CancellationToken cancellationToken);
 
-        // public Task StartAsync(CancellationToken cancellationToken)
-        // {
-        //     // Create a linked token so we can trigger cancellation outside of this token's cancellation
-        //     _cts = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
-
-        //     // Store the task we're executing
-        //     _executingTask = ExecuteAsync(_cts.Token);
-
-        //     // If the task is completed then return it, otherwise it's running
-        //     return _executingTask.IsCompleted ? _executingTask : Task.CompletedTask;
-        // }
-
-        // public async Task StopAsync(CancellationToken cancellationToken)
-        // {
-        //     // Stop called without start
-        //     if (_executingTask == null)
-        //     {
-        //         return;
-        //     }
-
-        //     // Signal cancellation to the executing method
-        //     _cts.Cancel();
-
-        //     // Wait until the task completes or the stop token triggers
-        //     await Task.WhenAny(_executingTask, Task.Delay(-1, cancellationToken));
-
-        //     // Throw if cancellation triggered
-        //     cancellationToken.ThrowIfCancellationRequested();
-        // }
 
         public static HostBuilder Create<TStartup>(string[] args) where TStartup : class
         {
@@ -102,7 +70,7 @@ namespace MushroomCloud.Common.Services
         public abstract class BuilderBase
         {
             public abstract HostedService Build();
-            
+
         }
 
         public class HostBuilder : BuilderBase
